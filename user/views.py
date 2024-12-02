754bda9ec411fe643979841e53cee5d8f53e74d0
seungwon0113 <jungseungwon0113@gmail.com>
2024-12-02 17:30:52 +0900

    swagger ui 사용자 tags 추가

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
+from jsonschema.validators import extend
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-
+from drf_spectacular.utils import extend_schema
 from .models import User
 from .serializers import UserSerializer, UserUpdateSerializer
 
@@ -10,6 +11,7 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
 
+    @extend_schema(tags=["사용자"])
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -32,14 +34,17 @@ class UserDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = UserUpdateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @extend_schema(tags=["사용자"])
     def get_object(self):
         return self.request.user
 
+    @extend_schema(tags=["사용자"])
     def get(self, request, *args, **kwargs):
         user = self.get_object()
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
+    @extend_schema(tags=["사용자"])
     def put(self, request, *args, **kwargs):
         user = self.get_object()
         serializer = self.get_serializer(user, data=request.data, partial=True)
@@ -56,6 +61,7 @@ class UserDeleteView(generics.DestroyAPIView):
     def get_object(self):
         return self.request.user
 
+    @extend_schema(tags=["사용자"])
     def delete(self, request, *args, **kwargs):
         user = self.get_object()
         user.is_active = False
