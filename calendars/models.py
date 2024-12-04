90475600cd46ac35924d38b725d74606c0fba1a2
grace287 <chenlucia287@gmail.com>
2024-12-04 16:19:37 +0900

    isort formatting

--- calendars/models.py
+++ calendars/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
+
 from user.models import User
+
 
 class Calendar(models.Model):
     calendar_id = models.BigIntegerField(primary_key=True)
