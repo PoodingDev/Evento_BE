d052f16ffe7ef95ccbac9c5dffc3b1f926176e38
grace287 <chenlucia287@gmail.com>
2024-12-04 14:20:02 +0900

    - 🐛fix : urls.py포매팅 적용

--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,9 @@ from .views import EventListCreateAPIView, EventRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path("events/", EventListCreateAPIView.as_view(), name="event-list-create"),
-    path("events/<int:pk>/", EventRetrieveUpdateDestroyAPIView.as_view(), name="event-detail"),
+    path(
+        "events/<int:pk>/",
+        EventRetrieveUpdateDestroyAPIView.as_view(),
+        name="event-detail",
+    ),
 ]
