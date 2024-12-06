9bac6de854af493a37a239a428458af0e1bb58c4
grace287 <chenlucia287@gmail.com>
2024-12-06 09:00:27 +0900

    - 💡chore : 공개, 비공개 url 추가

--- event/urls.py
+++ event/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
-from .views import EventListCreateAPIView, EventRetrieveUpdateDestroyAPIView
+from .views import (
+    PublicEventListAPIView,
+    PrivateEventListCreateAPIView,
+    EventRetrieveUpdateDestroyAPIView,
+)
 
 urlpatterns = [
-    path("", EventListCreateAPIView.as_view(), name="event-list-create"),
-    path(
-        "<int:pk>/",
-        EventRetrieveUpdateDestroyAPIView.as_view(),
-        name="event-detail",
-    ),
+    path("events/public/", PublicEventListAPIView.as_view(), name="public-event-list"),
+    path("events/private/", PrivateEventListCreateAPIView.as_view(), name="private-event-list-create"),
+    path("events/<int:pk>/", EventRetrieveUpdateDestroyAPIView.as_view(), name="event-detail"),
 ]
