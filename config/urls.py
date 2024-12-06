a3c32ca762129e052f4a8bb44700adf08f1f9640
grace287 <chenlucia287@gmail.com>
2024-12-06 11:08:43 +0900

    - 💡chore : event url update

--- config/urls.py
+++ config/urls.py
@@ -3,17 +3,19 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    # Admin URL
     path("admin/", admin.site.urls),
+    # Swagger 및 Schema 관련
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # User 관련
     path("api/users/", include("user.urls")),
+    # 캘린더 관련
     path('api/calendars/', include('calendars.urls')),  # 캘린더 앱 URL 등록
-    path("api/events/<int:event_id>/", include("comment.urls")),
+    # 이벤트 관련
+    path("api/events/", include("event.urls")),
+    # 댓글 관련 (특정 이벤트에 종속)
+    path("api/events/<int:event_id>/comments/", include("comment.urls")),
+    # 유저별 즐겨찾기 관련
     path("api/users/<str:user_id>/", include("favorite_event.urls")),
-    path("calendars/", include("calendars.urls")),
-    path("events/", include("event.urls")),
 ]
