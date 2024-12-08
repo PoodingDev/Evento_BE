d4dc5ef2ef64edc9c9ff4d1ca580d94d82a84c36
seungwon0113 <jungseungwon0113@gmail.com>
2024-12-09 01:45:54 +0900

    settings.py user/views.py 수정

--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
 import os
+import random
+import string
 
 import requests
 from django.contrib.auth import logout
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.utils import timezone
 from dotenv import load_dotenv
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics, permissions, status
+from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -15,6 +17,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
 from .serializers import UserSerializer, UserUpdateSerializer
+
+
+def generate_random_nickname():
+    return "".join(random.choices(string.ascii_letters + string.digits, k=8))
+
 
 load_dotenv()
 
@@ -32,6 +39,16 @@ class UserCreateView(generics.CreateAPIView):
 
     @extend_schema(tags=["사용자"])
     def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+
+        # 비활성화된 계정이 있는지 확인
+        try:
+            existing_user = User.objects.get(email=email, is_active=False)
+            # 기존 비활성 계정이 있다면 삭제
+            existing_user.delete()
+        except User.DoesNotExist:
+            pass
+
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -48,6 +65,40 @@ class UserCreateView(generics.CreateAPIView):
         return Response(serializer.errors, status=400)
 
 
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(tags=["사용자"])
+    def get_object(self):
+        if not self.request.user.is_authenticated:
+            raise AuthenticationFailed("인증되지 않은 사용자입니다.")
+        return self.request.user
+
+    @extend_schema(tags=["사용자"])
+    def delete(self, request, *args, **kwargs):
+        try:
+            user = self.get_object()
+            # 계정 비활성화
+            user.is_active = False
+            user.save()
+            # 로그아웃 처리
+            logout(request)
+            return Response(
+                {"message": "계정이 성공적으로 비활성화되었습니다."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except AuthenticationFailed:
+            return Response(
+                {"error": "인증에 실패했습니다. 다시 로그인해주세요."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+
 class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
@@ -56,47 +107,25 @@ class UserDetailView(generics.RetrieveUpdateAPIView):
     @extend_schema(tags=["사용자"])
     def get_object(self):
         if not self.request.user.is_authenticated:
-            return Response(
-                {
-                    "error": "인증 실패",
-                    "message": "로그인이 필요합니다. 다시 로그인해주세요.",
-                },
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
+            raise AuthenticationFailed("로그인이 필요합니다. 다시 로그인해주세요.")
 
         try:
             user = self.request.user
             if not user:
-                return Response(
-                    {
-                        "error": "사용자 정보 없음",
-                        "message": "현재 로그인된 사용자의 정보를 찾을 수 없습니다.",
-                    },
-                    status=status.HTTP_404_NOT_FOUND,
-                )
+                raise NotFound("현재 로그인된 사용자의 정보를 찾을 수 없습니다.")
             return user
         except User.DoesNotExist:
-            return Response(
-                {
-                    "error": "사용자 정보 없음",
-                    "message": "현재 로그인된 사용자의 정보를 찾을 수 없습니다.",
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            raise NotFound("현재 로그인된 사용자의 정보를 찾을 수 없습니다.")
 
     @extend_schema(tags=["사용자"])
     def get(self, request, *args, **kwargs):
         user = self.get_object()
-        if isinstance(user, Response):
-            return user
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
     @extend_schema(tags=["사용자"])
     def put(self, request, *args, **kwargs):
         user = self.get_object()
-        if isinstance(user, Response):  # 에러 응답인 경우
-            return user
 
         # 닉네임 중복 체크
         new_nickname = request.data.get("nickname")
@@ -106,35 +135,15 @@ class UserDetailView(generics.RetrieveUpdateAPIView):
             .exclude(user_id=user.user_id)
             .exists()
         ):
-            return Response(
-                {
-                    "error": "닉네임 중복",
-                    "message": "이미 사용 중인 닉네임입니다. 다른 닉네임을 선택해주세요.",
-                },
-                status=status.HTTP_400_BAD_REQUEST,
+            raise ValidationError(
+                "이미 사용 중인 닉네임입니다. 다른 닉네임을 선택해주세요."
             )
 
         serializer = self.get_serializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-
-
-class UserDeleteView(generics.DestroyAPIView):
-    queryset = User.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
-
-    @extend_schema(tags=["사용자"])
-    def get_object(self):
-        return self.request.user
-
-    @extend_schema(tags=["사용자"])
-    def delete(self, request, *args, **kwargs):
-        user = self.get_object()
-        user.is_active = False
-        user.save()
-        return Response(status=204)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LogoutView(APIView):
@@ -146,6 +155,10 @@ class LogoutView(APIView):
         return Response(
             {"message": "로그아웃이 완료되었습니다."}, status=status.HTTP_200_OK
         )
+
+
+def generate_random_nickname():
+    return "".join(random.choices(string.ascii_letters + string.digits, k=8))
 
 
 class GoogleLoginView(APIView):
@@ -176,7 +189,7 @@ class GoogleLoginView(APIView):
 
         response = requests.post(token_url, data=data)
         if response.status_code != 200:
-            return Response(
+            return JsonResponse(
                 {"error": "토큰 가져오기 실패"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
@@ -187,7 +200,7 @@ class GoogleLoginView(APIView):
         userinfo_response = requests.get(userinfo_url, headers=headers)
 
         if userinfo_response.status_code != 200:
-            return Response(
+            return JsonResponse(
                 {"error": "유저정보 가져오기 실패"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
@@ -198,16 +211,28 @@ class GoogleLoginView(APIView):
         try:
             user = User.objects.get(email=email)
             if not user.is_active:
-                return JsonResponse(
-                    {"error": "이 계정으로는 로그인할 수 없습니다."},
-                    status=status.HTTP_403_FORBIDDEN,
+                # 기존 계정 정보 저장
+                user_id = user.user_id
+                # 계정 삭제
+                user.delete()
+
+                # 새로운 계정 생성
+                new_user = User.objects.create_user(
+                    email=email,
+                    username=user_info.get("name", ""),
+                    birth=timezone.now().date(),
+                    nickname=generate_random_nickname(),
                 )
+                new_user.set_unusable_password()
+                new_user.save()
+                user = new_user
+            # 활성 계정인 경우 기존 사용자 사용
         except User.DoesNotExist:
             user = User.objects.create_user(
                 email=email,
                 username=user_info.get("name", ""),
                 birth=timezone.now().date(),
-                nickname=user_info.get("given_name", ""),
+                nickname=generate_random_nickname(),
             )
             user.set_unusable_password()
             user.save()
