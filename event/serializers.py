4bb4eaccf9bb13859492096320a86ed67730eb31
seungwon0113 <jungseungwon0113@gmail.com>
2024-12-10 04:00:08 +0900

    - fix : event 상세조회 수정

--- event/serializers.py
+++ event/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from favorite_event.models import FavoriteEvent
+
 from .models import Event
 
 
@@ -7,6 +9,8 @@ class EventSerializer(serializers.ModelSerializer):
     """
     Event 모델에 대한 기본 Serializer
     """
+
+    is_liked = serializers.BooleanField(source="get_is_liked", read_only=True)
 
     class Meta:
         model = Event
@@ -19,9 +23,23 @@ class EventSerializer(serializers.ModelSerializer):
             "end_time",
             "admin_id",
             "is_public",
+            "is_liked",
             "location",
         ]
         read_only_fields = ["event_id", "admin_id"]
+
+    def get_is_liked(self, obj):
+        """
+        현재 사용자가 이벤트를 좋아요 했는지 여부를 반환
+        True: 좋아요 한 상태
+        False: 좋아요 하지 않은 상태
+        """
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return FavoriteEvent.objects.filter(
+                user=request.user, event_id=obj.event_id
+            ).exists()  # True 또는 False 반환
+        return False
 
     def create(self, validated_data):
         validated_data["admin_id"] = self.context["request"].user
